905aceb65b47ef8514e4818e9de618df13d17dfc
bisaek <59936920+bisaek@users.noreply.github.com>
2025-10-22 18:42:47 +0200

    Enhance range manipulation in PokerRange component with multi-select functionality and improved action handling

--- src/components/range/range.svelte
+++ src/components/range/range.svelte
@@ -6,6 +6,8 @@
     HandStrings,
     PokerRangeLength,
     getButtonClass,
+    lineFromTwoHands,
+    handToVector,
   } from "../../utils/range.svelte";
 
   let {
@@ -18,11 +20,37 @@
     compareTo?: PokerRange;
   } = $props();
 
+  let startHand: Hand | undefined = $state(undefined);
+  let shiftDown: boolean = $state(false);
+  let line: number[][] = $state([]);
+
   function toggleHand(event: MouseEvent, index: number) {
     // console.log(event);
-    if (event.buttons !== 1) return; // Only proceed if left mouse button is pressed
-    pokerRange.range[index] = selectedAction;
-    //pokerRange = new PokerRange(pokerRange.name, [...pokerRange.range]); // Trigger reactivity
+    if (shiftDown) {
+      if (event.buttons !== 1) {
+        return;
+      }
+
+      if (startHand === undefined) {
+        startHand = index;
+      }
+
+      line = lineFromTwoHands(
+        Math.min(startHand, index),
+        Math.max(startHand, index)
+      );
+    } else {
+      if (event.buttons !== 1) return; // Only proceed if left mouse button is pressed
+      pokerRange.range[index] = selectedAction;
+      //pokerRange = new PokerRange(pokerRange.name, [...pokerRange.range]); // Trigger reactivity
+    }
+  }
+
+  function createLine(event: MouseEvent) {
+    if (startHand !== undefined) {
+      pokerRange.changeActionAtVectors(line, selectedAction);
+      startHand = undefined;
+    }
   }
 
   function getCompareClass(index: number): string {
@@ -37,16 +65,60 @@
         return "border-4 border-gray-300";
       case Action.AllIn:
         return "border-4 border-blue-500";
+      default:
+        return "";
+    }
+  }
+
+  function getHoverClass(index: number): string {
+    console.log(line);
+    if (startHand === undefined) return "";
+
+    if (
+      line.some(
+        (vec) =>
+          vec[0] === handToVector(index)[0] && vec[1] === handToVector(index)[1]
+      )
+    ) {
+      switch (pokerRange.range[index]) {
+        case Action.Fold:
+          return "bg-gray-400";
+        case Action.Call:
+          return "bg-green-600 text-white";
+        case Action.Raise:
+          return "bg-red-600 text-white";
+        case Action.AllIn:
+          return "bg-blue-600 text-white";
+        default:
+          return "bg-gray-200";
+      }
+    }
+    return "";
+  }
+
+  function keyDown(Event: KeyboardEvent) {
+    console.log(Event.key);
+    if (Event.key === "Shift") {
+      shiftDown = true;
+    }
+  }
+
+  function keyUp(Event: KeyboardEvent) {
+    if (Event.key === "Shift") {
+      shiftDown = false;
+      startHand = undefined;
     }
   }
 </script>
 
+<svelte:window onkeydown={keyDown} onkeyup={keyUp} />
 {#each Array(PokerRangeLength) as _, index}
   <!-- svelte-ignore a11y_mouse_events_have_key_events -->
   <button
-    class={`rounded ${getButtonClass(pokerRange.range[index])} ${getCompareClass(index)}`}
+    class={`rounded ${getButtonClass(pokerRange.range[index])} ${getCompareClass(index)} ${getHoverClass(index)}`}
     onmouseover={(e) => toggleHand(e, index)}
     onmousedown={(e) => toggleHand(e, index)}
+    onmouseup={createLine}
     title={HandStrings[index]}
   >
     {HandStrings[index]}
--- src/utils/range.svelte.ts
+++ src/utils/range.svelte.ts
@@ -1,78 +1,432 @@
 class PokerRange {
-    range: Action[];
-    name: string;
+  range: (Action | null)[];
+  name: string;
 
-    constructor(name: string = "My Range", range: Action[] = Array(PokerRangeLength).fill(Action.Fold)) {
-        this.range = $state(range);
-        this.name = $state(name);
-    }
+  constructor(
+    name: string = "My Range",
+    range: (Action | null)[] = Array(PokerRangeLength).fill(Action.Fold)
+  ) {
+    this.range = $state(range);
+    this.name = $state(name);
+  }
 
-    toJSON() {
-        return {
-            range: this.range,
-            name: this.name
-        }
-    }
+  changeActionAtVector(vector: number[], action: Action) {
+    const index = Math.floor(vector[0]) * 13 + Math.floor(vector[1]);
+    this.range[index] = action;
+  }
 
-    static fromJSON(json: { range: Action[]; name: string }): PokerRange {
-        return new PokerRange(json.name, json.range);
+  changeActionAtVectors(vectors: number[][], action: Action) {
+    for (const vector of vectors) {
+      this.changeActionAtVector(vector, action);
     }
+  }
+
+  toJSON() {
+    return {
+      range: this.range,
+      name: this.name,
+    };
+  }
+
+  static fromJSON(json: { range: Action[]; name: string }): PokerRange {
+    return new PokerRange(json.name, json.range);
+  }
 }
+function lineFromTwoHands(hand1: Hand, hand2: Hand): number[][] {
+  const range: number[][] = [];
+  const vec1 = handToVector(hand1);
+  const vec2 = handToVector(hand2);
 
+  console.log("vec1", vec1, "vec2", vec2);
+  const slope = (vec2[1] - vec1[1]) / (vec2[0] - vec1[0]);
+
+  let x = vec1[0];
+  while (x <= vec2[0]) {
+    const y = slope * (x - vec1[0]) + vec1[1];
+    range.push([Math.round(x), Math.round(y)]);
+    x += 1;
+  }
+  return range;
+}
 enum Action {
-    Raise = "Raise",
-    Call = "Call",
-    Fold = "Fold",
-    AllIn = "All in"
+  Raise = "Raise",
+  Call = "Call",
+  Fold = "Fold",
+  AllIn = "All in",
 }
 
 enum Hand {
-    AA, AKs, AQs, AJs, ATs, A9s, A8s, A7s, A6s, A5s, A4s, A3s, A2s,
-    AKo, KK, KQs, KJs, KTs, K9s, K8s, K7s, K6s, K5s, K4s, K3s, K2s,
-    AQo, KQo, QQ, QJs, QTs, Q9s, Q8s, Q7s, Q6s, Q5s, Q4s, Q3s, Q2s,
-    AJo, KJo, QJo, JJ, JTs, J9s, J8s, J7s, J6s, J5s, J4s, J3s, J2s,
-    ATo, KTo, QTo, JTo, TT, T9s, T8s, T7s, T6s, T5s, T4s, T3s, T2s,
-    A9o, K9o, Q9o, J9o, T9o, N99, N98s, N97s, N96s, N95s, N94s, N93s, N92s,
-    A8o, K8o, Q8o, J8o, T8o, N98o, N88, N87s, N86s, N85s, N84s, N83s, N82s,
-    A7o, K7o, Q7o, J7o, T7o, N97o, N87o, N77, N76s, N75s, N74s, N73s, N72s,
-    A6o, K6o, Q6o, J6o, T6o, N96o, N86o, N76o, N66, N65s, N64s, N63s, N62s,
-    A5o, K5o, Q5o, J5o, T5o, N95o, N85o, N75o, N65o, N55, N54s, N53s, N52s,
-    A4o, K4o, Q4o, J4o, T4o, N94o, N84o, N74o, N64o, N54o, N44, N43s, N42s,
-    A3o, K3o, Q3o, J3o, T3o, N93o, N83o, N73o, N63o, N53o, N43o, N33, N32s,
-    A2o, K2o, Q2o, J2o, T2o, N92o, N82o, N72o, N62o, N52o, N42o, N32o, N22
+  AA,
+  AKs,
+  AQs,
+  AJs,
+  ATs,
+  A9s,
+  A8s,
+  A7s,
+  A6s,
+  A5s,
+  A4s,
+  A3s,
+  A2s,
+  AKo,
+  KK,
+  KQs,
+  KJs,
+  KTs,
+  K9s,
+  K8s,
+  K7s,
+  K6s,
+  K5s,
+  K4s,
+  K3s,
+  K2s,
+  AQo,
+  KQo,
+  QQ,
+  QJs,
+  QTs,
+  Q9s,
+  Q8s,
+  Q7s,
+  Q6s,
+  Q5s,
+  Q4s,
+  Q3s,
+  Q2s,
+  AJo,
+  KJo,
+  QJo,
+  JJ,
+  JTs,
+  J9s,
+  J8s,
+  J7s,
+  J6s,
+  J5s,
+  J4s,
+  J3s,
+  J2s,
+  ATo,
+  KTo,
+  QTo,
+  JTo,
+  TT,
+  T9s,
+  T8s,
+  T7s,
+  T6s,
+  T5s,
+  T4s,
+  T3s,
+  T2s,
+  A9o,
+  K9o,
+  Q9o,
+  J9o,
+  T9o,
+  N99,
+  N98s,
+  N97s,
+  N96s,
+  N95s,
+  N94s,
+  N93s,
+  N92s,
+  A8o,
+  K8o,
+  Q8o,
+  J8o,
+  T8o,
+  N98o,
+  N88,
+  N87s,
+  N86s,
+  N85s,
+  N84s,
+  N83s,
+  N82s,
+  A7o,
+  K7o,
+  Q7o,
+  J7o,
+  T7o,
+  N97o,
+  N87o,
+  N77,
+  N76s,
+  N75s,
+  N74s,
+  N73s,
+  N72s,
+  A6o,
+  K6o,
+  Q6o,
+  J6o,
+  T6o,
+  N96o,
+  N86o,
+  N76o,
+  N66,
+  N65s,
+  N64s,
+  N63s,
+  N62s,
+  A5o,
+  K5o,
+  Q5o,
+  J5o,
+  T5o,
+  N95o,
+  N85o,
+  N75o,
+  N65o,
+  N55,
+  N54s,
+  N53s,
+  N52s,
+  A4o,
+  K4o,
+  Q4o,
+  J4o,
+  T4o,
+  N94o,
+  N84o,
+  N74o,
+  N64o,
+  N54o,
+  N44,
+  N43s,
+  N42s,
+  A3o,
+  K3o,
+  Q3o,
+  J3o,
+  T3o,
+  N93o,
+  N83o,
+  N73o,
+  N63o,
+  N53o,
+  N43o,
+  N33,
+  N32s,
+  A2o,
+  K2o,
+  Q2o,
+  J2o,
+  T2o,
+  N92o,
+  N82o,
+  N72o,
+  N62o,
+  N52o,
+  N42o,
+  N32o,
+  N22,
+}
+
+function handToVector(hand: Hand): number[] {
+  return [Math.floor(hand / 13), hand % 13];
 }
 
 const HandStrings: string[] = [
-    "AA", "AKs", "AQs", "AJs", "ATs", "A9s", "A8s", "A7s", "A6s", "A5s", "A4s", "A3s", "A2s",
-    "AKo", "KK", "KQs", "KJs", "KTs", "K9s", "K8s", "K7s", "K6s", "K5s", "K4s", "K3s", "K2s",
-    "AQo", "KQo", "QQ", "QJs", "QTs", "Q9s", "Q8s", "Q7s", "Q6s", "Q5s", "Q4s", "Q3s", "Q2s",
-    "AJo", "KJo", "QJo", "JJ", "JTs", "J9s", "J8s", "J7s", "J6s", "J5s", "J4s", "J3s", "J2s",
-    "ATo", "KTo", "QTo", "JTo", "TT", "T9s", "T8s", "T7s", "T6s", "T5s", "T4s", "T3s", "T2s",
-    "A9o", "K9o", "Q9o", "J9o", "T9o", "99", "98s", "97s", "96s", "95s", "94s", "93s", "92s",
-    "A8o", "K8o", "Q8o", "J8o", "T8o", "98o", "88", "87s", "86s", "85s", "84s", "83s", "82s",
-    "A7o", "K7o", "Q7o", "J7o", "T7o", "97o", "87o", "77", "76s", "75s", "74s", "73s", "72s",
-    "A6o", "K6o", "Q6o", "J6o", "T6o", "96o", "86o", "76o", "66", "65s", "64s", "63s", "62s",
-    "A5o", "K5o", "Q5o", "J5o", "T5o", "95o", "85o", "75o", "65o", "55", "54s", "53s", "52s",
-    "A4o", "K4o", "Q4o", "J4o", "T4o", "94o", "84o", "74o", "64o", "54o", "44", "43s", "42s",
-    "A3o", "K3o", "Q3o", "J3o", "T3o", "93o", "83o", "73o", "63o", "53o", "43o", "33", "32s",
-    "A2o", "K2o", "Q2o", "J2o", "T2o", "92o", "82o", "72o", "62o", "52o", "42o", "32o", "22"
+  "AA",
+  "AKs",
+  "AQs",
+  "AJs",
+  "ATs",
+  "A9s",
+  "A8s",
+  "A7s",
+  "A6s",
+  "A5s",
+  "A4s",
+  "A3s",
+  "A2s",
+  "AKo",
+  "KK",
+  "KQs",
+  "KJs",
+  "KTs",
+  "K9s",
+  "K8s",
+  "K7s",
+  "K6s",
+  "K5s",
+  "K4s",
+  "K3s",
+  "K2s",
+  "AQo",
+  "KQo",
+  "QQ",
+  "QJs",
+  "QTs",
+  "Q9s",
+  "Q8s",
+  "Q7s",
+  "Q6s",
+  "Q5s",
+  "Q4s",
+  "Q3s",
+  "Q2s",
+  "AJo",
+  "KJo",
+  "QJo",
+  "JJ",
+  "JTs",
+  "J9s",
+  "J8s",
+  "J7s",
+  "J6s",
+  "J5s",
+  "J4s",
+  "J3s",
+  "J2s",
+  "ATo",
+  "KTo",
+  "QTo",
+  "JTo",
+  "TT",
+  "T9s",
+  "T8s",
+  "T7s",
+  "T6s",
+  "T5s",
+  "T4s",
+  "T3s",
+  "T2s",
+  "A9o",
+  "K9o",
+  "Q9o",
+  "J9o",
+  "T9o",
+  "99",
+  "98s",
+  "97s",
+  "96s",
+  "95s",
+  "94s",
+  "93s",
+  "92s",
+  "A8o",
+  "K8o",
+  "Q8o",
+  "J8o",
+  "T8o",
+  "98o",
+  "88",
+  "87s",
+  "86s",
+  "85s",
+  "84s",
+  "83s",
+  "82s",
+  "A7o",
+  "K7o",
+  "Q7o",
+  "J7o",
+  "T7o",
+  "97o",
+  "87o",
+  "77",
+  "76s",
+  "75s",
+  "74s",
+  "73s",
+  "72s",
+  "A6o",
+  "K6o",
+  "Q6o",
+  "J6o",
+  "T6o",
+  "96o",
+  "86o",
+  "76o",
+  "66",
+  "65s",
+  "64s",
+  "63s",
+  "62s",
+  "A5o",
+  "K5o",
+  "Q5o",
+  "J5o",
+  "T5o",
+  "95o",
+  "85o",
+  "75o",
+  "65o",
+  "55",
+  "54s",
+  "53s",
+  "52s",
+  "A4o",
+  "K4o",
+  "Q4o",
+  "J4o",
+  "T4o",
+  "94o",
+  "84o",
+  "74o",
+  "64o",
+  "54o",
+  "44",
+  "43s",
+  "42s",
+  "A3o",
+  "K3o",
+  "Q3o",
+  "J3o",
+  "T3o",
+  "93o",
+  "83o",
+  "73o",
+  "63o",
+  "53o",
+  "43o",
+  "33",
+  "32s",
+  "A2o",
+  "K2o",
+  "Q2o",
+  "J2o",
+  "T2o",
+  "92o",
+  "82o",
+  "72o",
+  "62o",
+  "52o",
+  "42o",
+  "32o",
+  "22",
 ];
 
 const PokerRangeLength = 13 * 13;
 
-function getButtonClass(action: Action): string {
-    switch (action) {
-      case Action.Fold:
-        return "bg-gray-300 hover:bg-gray-400";
-      case Action.Call:
-        return "bg-green-500 hover:bg-green-600 text-white";
-      case Action.Raise:
-        return "bg-red-500 hover:bg-red-600 text-white";
-      case Action.AllIn:
-        return "bg-blue-500 hover:bg-blue-600 text-white";
-      default:
-        return "bg-gray-100 hover:bg-gray-200";
-    }
+function getButtonClass(action: Action | null): string {
+  switch (action) {
+    case Action.Fold:
+      return "bg-gray-300 hover:bg-gray-400";
+    case Action.Call:
+      return "bg-green-500 hover:bg-green-600 text-white";
+    case Action.Raise:
+      return "bg-red-500 hover:bg-red-600 text-white";
+    case Action.AllIn:
+      return "bg-blue-500 hover:bg-blue-600 text-white";
+    default:
+      return "bg-gray-100 hover:bg-gray-200";
   }
+}
 
-export { PokerRange, Action, Hand, HandStrings, PokerRangeLength, getButtonClass };
+export {
+  PokerRange,
+  Action,
+  Hand,
+  HandStrings,
+  PokerRangeLength,
+  getButtonClass,
+  lineFromTwoHands,
+  handToVector,
+};
